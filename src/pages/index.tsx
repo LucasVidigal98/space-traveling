416e9137355182a662ef8a574bc79219ec2ead63
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-01-19 23:49:29 -0300

    Get posts by prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { GetStaticProps } from 'next';
 import { FaCalendar, FaUser } from 'react-icons/fa';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -25,62 +27,64 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   return (
     <main className={styles.homeContainer}>
       <section className={styles.homeContent}>
-        <div className={styles.post}>
-          <h1> Como utilizar Hooks </h1>
-          <p>Pensando em sincronização em vez de ciclos de vida</p>
-          <div>
-            <time>
-              <FaCalendar className={styles.icon} /> 15 Mar 2021
-            </time>
-            <span>
-              <FaUser className={styles.icon} />
-              Lucas
-            </span>
+        {postsPagination.results.map(post => (
+          <div key={post.uid} className={styles.post}>
+            <h1> {post.data.title} </h1>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <time>
+                <FaCalendar className={styles.icon} /> { post.first_publication_date }
+              </time>
+              <span>
+                <FaUser className={styles.icon} />
+                {post.data.author}
+              </span>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.post}>
-          <h1> Como utilizar Hooks </h1>
-          <p>Pensando em sincronização em vez de ciclos de vida</p>
-          <div>
-            <time>
-              <FaCalendar className={styles.icon} /> 15 Mar 2021
-            </time>
-            <span>
-              <FaUser className={styles.icon} />
-              Lucas
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.post}>
-          <h1> Como utilizar Hooks </h1>
-          <p>Pensando em sincronização em vez de ciclos de vida</p>
-          <div>
-            <time>
-              <FaCalendar className={styles.icon} /> 15 Mar 2021
-            </time>
-            <span>
-              <FaUser className={styles.icon} />
-              Lucas
-            </span>
-          </div>
-        </div>
+        ))}
       </section>
     </main>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  // const prismic = getPrismicClient();
-  // const postsResponse = await prismic.query(TODO);
-  // TODO
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.slug', 'post.title', 'post.subtitle', 'post.author'],
+      pageSize: 100,
+    }
+  );
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: [],
+  };
+
+  postsPagination.results = postsResponse.results.map(postData => {
+    return {
+      uuid: postData.uid,
+      first_publication_date: new Date(postData.first_publication_date.toString()).toLocaleString('pt-BR', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+      }),
+      data: {
+        title: postData.data.title,
+        subtitle: postData.data.subtitle,
+        author: postData.data.author,
+      },
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      postsPagination,
+    },
   };
 };
