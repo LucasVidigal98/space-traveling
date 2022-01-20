052db371869eb111329faeaa86b705c1d887f813
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-01-19 22:06:47 -0300

    Created Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <img src="/images/Logo.svg" alt="Logo" />
+
+          <h1>
+            spacetraveling<span>.</span>
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
 }
